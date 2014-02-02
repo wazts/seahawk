072e8fce70ac99341056f8181406ef710c7ab3db
wazts <tskazion@gmail.com>
2014-02-02 17:40:02 -0600

    Fixes to the settings of the REST API

--- server/rest/tags/urls.py
+++ server/rest/tags/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns ('tags.views',
 url(r'^tags/$', 'tag_list'),
 url(r'^tags/(?P<pk>[0-9]+)/$', 'tag_detail'),
 
 )
+
+urlpatterns = format_suffix_patterns (urlpatterns);
--- server/rest/tags/views.py
+++ server/rest/tags/views.py
@@ -1,61 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from tags.models import Tag
 from tags.serializers import TagSerializer
 
-class JSONResponse (HttpResponse):
-	"""
-	An HttpResponse that renders its content to JSON
-	"""
 
-	def __init__ (self, data, **kwargs):
-		content = JSONRenderer().render(data)
-		kwargs ['content_type'] = 'application/json'
-		super (JSONResponse, self).__init__(content, **kwargs)
-
-@csrf_exempt
-def tag_list (request):
+@api_view (['GET', 'POST'])
+def tag_list(request, format=None):
 	"""
 	List all tags, or push a new tag
 	"""
 	if request.method == 'GET':
 		tags = Tag.objects.all()
 		serializer = TagSerializer (tags, many=True)
-		return JSONResponse (serializer.data)
+		return Response (serializer.data)
 
 	elif request.method == 'POST':
-		data = JSONParser().parse(request)
-		serializer = TagSerializer (data=data)
+		serializer = TagSerializer (data=request.DATA)
 		if serializer.is_valid():
 			serializer.save()
-			return JSONResponse (serializer.data, status=201)
-		return JSONResponse (serializer.errors, status=400)
+			return Response (serializer.data, status=status.HTTP_201_CREATED)
+		return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def tag_detail (request, pk):
+@api_view (['GET', 'PUT', 'DELETE'])
+def tag_detail (request, pk, format=None):
 	"""
 	Retrieve, update or delete a tag.
 	"""
 	try:
 		tag = Tag.objects.get (pk=pk)
 	except Tag.DoesNotExist:
-		return HttpResponse (status=404)
+		return Response (status=status.HTTP_404_NOT_FOUND)
 
 	if request.method == 'GET':
 		serializer = TagSerializer (tag)
-		return JSONResponse (serializer.data)
+		return Response (serializer.data)
 
 	if request.method == 'PUT':
-		data = JSONParser().parse(request)
-		serializer = TagSerializer (tag, data=data)
+		serializer = TagSerializer (tag, data=request.DATA)
 		if serializer.is_valid():
 			serializer.save()
-			return JSONResponse (serializer.data)
-		return JSONResponse (serializer.errors, status=400)
+			return Response (serializer.data)
+		return Resposne (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	elif request.method == 'DELETE':
 		tag.delete()
-		return HttpResponse(status=204)
+		return Response(status=status.HTTP_204_NO_CONTENT)
